d6bd75b0e765ccd63d7dbf1454c998ba54a9a132
sandesh6190 <sandesh0842@gmail.com>
2024-04-30 19:49:42 +0545

    Understanding State and event handlings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+
 
 function App() {
   return (
     <>
     <Navbar title="Text Utils" aboutText="About TextUtils" />
     {/* <Navbar /> */}
+    <div className='container'>
+    <TextForm heading="Enter the text to anlyze"/>
+    </div>
 </>
   );
 }
---
+++ src/Components/TextForm.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+
+  const handleUpClick = () =>{
+    console.log(text);
+    let upText = text.toUpperCase();
+    setText(upText);
+  }
+
+  const handleLwClick = () =>{
+    let lwText = text.toLowerCase();
+    setText(lwText);
+  }
+
+  const handleOnChange = (event) =>{
+    setText(event.target.value)
+  }
+  const [text, setText] = useState('');
+
+  return (
+    <div>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+        <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
+        </div>
+        <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to uppercase</button>
+        <button className='btn btn-warning' onClick={handleLwClick}>Convert to lowercase</button>
+        
+        <div className='container my-3'>
+          <h1>Your Text Summary</h1>
+          <p> {text.split(" ").length} words and {text.length} characters</p>
+          <p>{0.008 * text.split(" ").length} minutes read</p>
+          <h2>Preview</h2>  
+          <p>{text}</p>
+        </div>
+    </div>
+
+  );
+}
