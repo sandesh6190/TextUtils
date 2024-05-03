5868757b49c42928d3dbcd25788fc582bf51c7f0
sandesh6190 <sandeshlimbu0842@gmail.com>
2024-05-03 19:04:13 +0545

    react-router-dom setup and usage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { useState } from 'react';
 import './App.css';
-// import About from './Components/About';
 import Navbar from './Components/Navbar';
 import Alert from './Components/Alert';
 import TextForm from './Components/TextForm';
+import About from './Components/About';
+
+import {
+  BrowserRouter as Router, //but createBrowserrouter is implemented in official react-router-dom site
+  Routes, //Routes has replaced the Switch 
+  Route
+  // Link isn't implemented here
+} from "react-router-dom";
 
 function App() {
-  const toggleMode = ()=> {
-    if(mode === 'light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#092540';
       showAlert("Dark mode has been enabled.", "success");
       //setting title 
-      document.title = "Text Utils- Dark Mode"; 
+      document.title = "Text Utils- Dark Mode";
       //blinking title
       // setInterval(() => {
       // document.title = "Text Util is amazing";
@@ -22,36 +29,44 @@ function App() {
       //   document.title = "Instal Text Util now";
       // },1500)
     }
-    else{
+    else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled.", "success");
       document.title = "Text Utils- Light Mode";
     }
   }
-  const showAlert = (message, type) =>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
     })
 
-    setTimeout(()=>{    //built-in function
+    setTimeout(() => {    //built-in function
       setAlert(null)
-    },3000)
+    }, 3000)
   }
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
 
   return (
     <>
-    <Navbar title="Text Utils" mode={mode} toggleMode={toggleMode} aboutText="About TextUtils" /> 
-    {/* toggleMode is passed only to call from navbar.js */}
-    <Alert alert={alert} />
-    <div className='container'>
-    <TextForm mode={mode} showAlert={showAlert} heading="Enter the text to anlyze"/>
-    {/* <About /> */}
-    </div>
-</>
+      <Router>
+        <Navbar title="Text Utils" mode={mode} toggleMode={toggleMode} aboutText="About" />
+        {/* toggleMode is passed only to call from navbar.js */}
+        <Alert alert={alert} />
+        <div className='container'>
+          <Routes>
+            {/* exact path defines the exact path as
+               /users ->Component 1
+               /users/details ->Component2*/}
+            <Route exact path="/" element={<TextForm mode={mode} showAlert={showAlert} heading="Enter the text to anlyze" />} />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </Router >
+
+    </>
   );
 }
 
